624d3fdc5b2889cf58a2e1a11d52a57da134cd3a
santhosh kodam <santoshkodam99@gmail.com>
2023-08-15 22:09:37 +0530

    changes for alt logo  txt

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo santhos" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. dhsehdjs
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener dsgdhsg noreferrer"
         >
           Learn React
         </a>
